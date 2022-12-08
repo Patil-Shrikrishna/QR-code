632bd9b49b64a6e976624cc5af3f4cd4571ef8ac
Patil-Shrikrishna <krishnapt1993@gmail.com>
2022-12-08 22:39:19 +0530

    first sprint

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Card from '@mui/material/Card';
+import './App.css';
+
+function App() {
+  // const qrCode = "images/image-qr-code.png"
+  return (
+    <div className="App">
+        <Card className='qr-card'>
+          <img className='qr-image' src="image-qr-code.png" alt='qr-code'/>
+          <h1 className="qr-title">
+            Improve your front-end skills by building projects.
+          </h1>
+          <h1 className="qr-description">  
+            Scan the QR code to visit frontend mentor and take your coding skills to the next level.
+          </h1>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
